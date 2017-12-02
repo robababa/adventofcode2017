69df3d9c7e81389a521af120f0b1eea67725004f
Rob Johnson <robj@hightouchinc.com>
2017-12-02 11:46:45 -0600

    Solved day 2 part 2

--- day02/day02.go
+++ day02/day02.go
@@ -14,6 +14,7 @@ Read lines of input into an array of strings until we read a . character on a li
 Convert the array of strings into an two-dimensional array of integers
 Read each row of integers to find the smallest and largest
 Add up these differences for the answer
+For part 2, use a different algorithm
 */
 
 func main() {
@@ -22,18 +23,20 @@ func main() {
 	//fmt.Println("len(stringInput) is", len(stringInput))
 	spreadsheet := convertInput(stringInput)
 	fmt.Println("spreadsheet is", spreadsheet)
-	fmt.Println("row-based checksum is", computeRowBasedChecksum(spreadsheet))
+	fmt.Println("MinMax checksum is", computeChecksum(spreadsheet, computeMinMaxChecksum))
+	fmt.Println("Divisor checksum is", computeChecksum(spreadsheet, computeDivisorChecksum))
 }
 
-func computeRowBasedChecksum(spreadsheet [][]int) int {
+func computeChecksum(spreadsheet [][]int, rowChecksum func([]int) int) int {
 	answer := 0
 	for _, row := range spreadsheet {
-		answer += computeRowChecksum(row)
+		answer += rowChecksum(row)
 	}
 	return answer
 }
 
-func computeRowChecksum(row []int) int {
+// part 1
+func computeMinMaxChecksum(row []int) int {
 	starting := true
 	var minValue int
 	var maxValue int
@@ -48,6 +51,23 @@ func computeRowChecksum(row []int) int {
 		}
 	}
 	return maxValue - minValue
+}
+
+// part 2
+func computeDivisorChecksum(row []int) int {
+	for i, num1 := range row {
+		for _, num2 := range row[(i + 1):] {
+			if num1%num2 == 0 {
+				//fmt.Println("Found numbers", num1, num2)
+				return num1 / num2
+			} else if num2%num1 == 0 {
+				//fmt.Println("Found numbers", num2, num1)
+				return num2 / num1
+			}
+		}
+	}
+	fmt.Println("Could not find numbers that divide each other in row:", row)
+	return 0
 }
 
 func convertInput(inputSlice []string) [][]int {
