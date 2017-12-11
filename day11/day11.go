ef2fe8fca687f4c29bedd4baee11a7c87c0871ee
Rob Johnson <robj@hightouchinc.com>
2017-12-11 09:45:37 -0600

    Solved day 11 parts 1 and 2 entirely
    i.e. no manual computation at the end

--- day11/day11.go
+++ day11/day11.go
@@ -4,37 +4,76 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	"math"
 	"strings"
 )
 
 var m = make(map[string]int)
 
+var realPart = 0
+var imaginaryPart = 0
+
 func main() {
 	parseInput(readInput())
-	printResults()
-	cancelOpposites("n", "s")
-	cancelOpposites("ne", "sw")
-	cancelOpposites("nw", "se")
-	printResults()
+	//printParts()
+	fmt.Println("Part 1: Distance is", computeDistance())
 }
 
-func cancelOpposites(direction1 string, direction2 string) {
-	if m[direction1] >= m[direction2] {
-		m[direction1] -= m[direction2]
-		m[direction2] = 0
+func computeDistance() int {
+	absRealPart := int(math.Abs(float64(realPart)))
+	absImaginaryPart := int(math.Abs(float64(imaginaryPart)))
+	// if the real part is greater, then we can move adjust the imaginary part up and down as needed without making
+	// any extra moves
+	// else the imaginary part is greater, and we can move along the diagonal "realPart" times, and then move up or down
+	// as needed, but each move up or down adjusts our imaginary part by 2, so we divide the difference by 2 to get
+	// the number of remaining moves
+	if absRealPart > absImaginaryPart {
+		return absRealPart
 	} else {
-		cancelOpposites(direction2, direction1)
+		return absRealPart + (absImaginaryPart - absRealPart)/2
 	}
 }
 
-func printResults() {
-	fmt.Println(m)
-}
+//func printParts() {
+//	fmt.Println("realPart is", realPart, "and imaginaryPart is", imaginaryPart)
+//}
 
 func parseInput(str string) {
+	greatestDistance := 0
+	currentDistance := 0
 	for _, c := range strings.Split(str, ",") {
-		m[c]++
+		switch c {
+		case "n":
+			imaginaryPart += 2
+		case "s":
+			imaginaryPart -= 2
+		case "ne":
+			{
+				realPart += 1
+				imaginaryPart += 1
+			}
+		case "nw":
+			{
+				realPart -= 1
+				imaginaryPart += 1
+			}
+		case "sw":
+			{
+				realPart -= 1
+				imaginaryPart -= 1
+			}
+		case "se":
+			{
+				realPart += 1
+				imaginaryPart -= 1
+			}
+		}
+		currentDistance = computeDistance()
+		if currentDistance > greatestDistance {
+			greatestDistance = currentDistance
+		}
 	}
+	fmt.Println("Part 2: Greatest distance is", greatestDistance)
 }
 
 func readInput() string {
