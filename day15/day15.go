c95c9cbbc092d081642c4110ce070547bca6dd14
Rob Johnson <robj@hightouchinc.com>
2017-12-15 08:20:22 -0600

    Solved day 15 parts 1 and 2

--- day15/day15.go
+++ day15/day15.go
@@ -8,16 +8,22 @@ const StartB = 349 // 8921
 const FactorA = 16807
 const FactorB = 48271
 const Modulus = 2147483647
-const NumRounds = 40000000
+const Part1Rounds = 40000000
+const Part2Rounds = 5000000
 const compareModulus = 65536
 
 func main() {
+	part1()
+	part2()
+}
+
+func part2() {
 	numA := StartA
 	numB := StartB
 	similarCount := 0
-	for generatorRound := 1; generatorRound <= NumRounds; generatorRound++ {
-		numA = numA * FactorA % Modulus
-		numB = numB * FactorB % Modulus
+	for generatorRound := 1; generatorRound <= Part2Rounds; generatorRound++ {
+		numA = generateNextA(numA, true)
+		numB = generateNextB(numB, true)
 		//fmt.Println("A is", numA, "and B is", numB)
 		if (numA - numB) % compareModulus == 0 {
 			//fmt.Println("A and B are similar at the end")
@@ -25,4 +31,49 @@ func main() {
 		}
 	}
 	fmt.Println("Part 1: similar random number count is", similarCount)
+}
+
+
+func part1() {
+	numA := StartA
+	numB := StartB
+	similarCount := 0
+	for generatorRound := 1; generatorRound <= Part1Rounds; generatorRound++ {
+		numA = generateNextA(numA, false)
+		numB = generateNextB(numB, false )
+		//fmt.Println("A is", numA, "and B is", numB)
+		if (numA - numB) % compareModulus == 0 {
+			//fmt.Println("A and B are similar at the end")
+			similarCount++
+		}
+	}
+	fmt.Println("Part 2: similar random number count is", similarCount)
+}
+
+func generateNextA(currentA int, beChoosy bool) int {
+	if !beChoosy {
+		return currentA * FactorA % Modulus
+	}
+	answer := currentA * FactorA % Modulus
+	for {
+		if answer % 4 == 0 {
+			return answer
+		} else {
+			answer = answer * FactorA % Modulus
+		}
+	}
+}
+
+func generateNextB(currentB int, beChoosy bool) int {
+	if !beChoosy {
+		return currentB * FactorB % Modulus
+	}
+	answer := currentB * FactorB % Modulus
+	for {
+		if answer % 8 == 0 {
+			return answer
+		} else {
+			answer = answer * FactorB % Modulus
+		}
+	}
 }
