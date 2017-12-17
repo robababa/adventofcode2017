3ff379e87411f638e3a7ca6a6e1e77d78e011e0e
Rob Johnson <robj@hightouchinc.com>
2017-12-17 00:11:34 -0600

    Solved day 17 parts 1 and 2

--- day17/day17.go
+++ day17/day17.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -18,10 +19,30 @@ func part1() {
 		r = r.Move(steps)
 		s := ring.New(1)
 		s.Value = val
-		//fmt.Println("s.Value is", s.Value)
 		r.Link(s)
 		r = r.Next()
-		//fmt.Println("r.Value is", r.Value)
 	}
-	fmt.Println(r.Next().Value)
+	fmt.Println("Part 1: Next value is:", r.Next().Value)
+}
+
+func part2() {
+	r := ring.New(1)
+	r.Value = 0
+	iterations := 50000000
+	steps := 386
+	for val := 1; val <= iterations; val++ {
+		r = r.Move(steps)
+		s := ring.New(1)
+		s.Value = val
+		r.Link(s)
+		r = r.Next()
+	}
+	for {
+		if r.Value == 0 {
+			fmt.Println("Part 2: Value after 0 is:", r.Next().Value)
+			break
+		} else {
+			r = r.Next()
+		}
+	}
 }
