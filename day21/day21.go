126a996c9e8ce0b93322ceca83624e39c5c28756
Rob Johnson <robj@hightouchinc.com>
2017-12-21 20:08:40 -0600

    I have flip and rotate functions, but need tests.

--- day21/day21.go
+++ day21/day21.go
@@ -9,11 +9,69 @@ import (
 
 // an enhancement is a mapping like this
 // .#/#. => ##./###/...
+// the key is the string on the left, and the value is the string on the right
 var enhancements = make(map[string]string)
+
+// the initial value is
+// .#.
+// ..#
+// ###
+var InitialGrid = []string{".#.", "..#", "###"}
 
 func main() {
 	parseInput(readInput())
-	fmt.Println(enhancements)
+	//fmt.Println(enhancements)
+	fmt.Println(enhance(InitialGrid))
+}
+
+func rotateClockwise(grid []string) []string {
+	var answer []string
+	// "prime" our array of strings with empty strings
+	for range grid {
+		answer = append(answer, "")
+	}
+	// now append to each of the empty strings
+	// example:  in the grid
+	// 1 2
+	// 3 4
+	// we visit 3 and 1 to fill in the top row,
+	// then visit 4 and 2 to fill in the bottom row
+	for i := len(grid) -1; i >= 0; i-- {
+		for j := 0; j <= len(grid) -1; j++ {
+			answer[j] += string(grid[i][j])
+		}
+	}
+	return answer
+}
+
+func flipTopAndBottom(grid []string) []string {
+	var answer []string
+	for _, s := range grid {
+		answer = append([]string{s}, answer...)
+	}
+	return answer
+}
+
+func flipLeftAndRight(grid []string) []string {
+	var answer []string
+	for i := len(grid[0]) -1; i>=0; i-- {
+		var nextLine string
+		for j := 0; j < len(grid[0]); j++ {
+			nextLine += string(grid[i][j])
+		}
+		answer = append(answer, nextLine)
+	}
+	return answer
+}
+
+func enhance(grid []string) []string {
+	var answer []string
+	enhancementKey := grid[0] + "/" + grid[1]
+	enhancementValue := enhancements[enhancementKey]
+	for _, str := range strings.Split(enhancementValue, "/") {
+		answer = append(answer, str)
+	}
+	return answer
 }
 
 func parseInput(lines []string) {
