ec9ae678af04e16e2c325c38d7edfce00f6a0aa2
Rob Johnson <robj@hightouchinc.com>
2017-12-23 14:32:52 -0600

    It runs the sample input, but answer is incorrect.

--- day20/day20_part2.go
+++ day20/day20_part2.go
@@ -7,15 +7,14 @@ import (
 	"strings"
 	"strconv"
 	"log"
-	"math"
+	"github.com/robababa/quadratic"
 )
-
-const NoSolutionValue = -1
-const AlwaysZero = -2
 
 type Coordinate struct {
 	x,y,z int
 }
+
+const NotYet = -1
 
 type Particle struct {
 	// p=position, v=velocity, a=acceleration
@@ -31,7 +30,7 @@ type Collision struct {
 
 var collisions = make(map[int][]Collision)
 
-var latestCollisionRound = 0
+var latestCollisionRound = NotYet
 
 func main() {
 	fmt.Println("Starting day 20 part 2...")
@@ -41,12 +40,16 @@ func main() {
 	loadCollisions(particles)
 	removeParticles(particles)
 	fmt.Println("The number of particles remaining is", countRemainingParticles(particles))
+	fmt.Println("Particles are:")
+	fmt.Println(particles)
+	fmt.Println("Collisions are:")
+	fmt.Println(collisions)
 }
 
 func countRemainingParticles(particles []Particle) int {
 	answer := 0
 	for _, p := range particles {
-		if p.zappedRound == 0 {
+		if p.zappedRound == NotYet {
 			answer++
 		}
 	}
@@ -55,8 +58,10 @@ func countRemainingParticles(particles []Particle) int {
 
 func removeParticles(particles []Particle) {
 	// loop through the collisions each round
-	for i := 1; i <= latestCollisionRound; i++ {
+	for i := 0; i <= latestCollisionRound; i++ {
 		for _, collision := range collisions[i] {
+			// if both particles either have NEVER been zapped, or are zapped THIS round, then they can
+			// zap each other
 			if (particles[collision.p1num].zappedRound == 0 || particles[collision.p1num].zappedRound == i) &&
 				(particles[collision.p2num].zappedRound == 0 || particles[collision.p2num].zappedRound == i) {
 				particles[collision.p1num].zappedRound = i
@@ -66,142 +71,56 @@ func removeParticles(particles []Particle) {
 	}
 }
 
-// returns the positive integer solution to the equation bx + c = 0, if one exists
-// if it is always true for all positive x, then it returns AlwaysZero
-// if it is true for no positive integers x, then it returns NoSolutionValue
-func linearPositiveIntegerSolutions(b int, c int) (int, int) {
-	switch {
-	// 0 = 0 is always true
-	case b == 0 && c == 0:
-		return AlwaysZero, AlwaysZero
-		// c == 0 is always false when c != 0
-	case b == 0 && c != 0:
-		return NoSolutionValue, NoSolutionValue
-		// bx = 0 for b != 0 is true iff x is 0
-	case b != 0 && c == 0:
-		return 0, NoSolutionValue
-	default: {
-		possibleAnswer := int(-1 * c / b)
-		if possibleAnswer > 0 && b*possibleAnswer+c == 0 {
-			return possibleAnswer, NoSolutionValue
-		} else {
-			return NoSolutionValue, NoSolutionValue
-		}
-	}
-	}
-}
-
-// returns the positive integer solutions to the quadratic equation
-// ax^2 + bx + c = 0
-// if there are two solutions, this function returns both of them
-// if there is only one positive integer solution, it returns that solution and NoSolutionValue
-// if there are no positive integer solutions, the function returns NoSolutionValue, NoSolutionValue
-func quadraticPositiveIntegerSolutions(a int, b int, c int) (int, int) {
-	fmt.Println("Solving quadratic equation", a, "* t^2 +", b, "* t +", c, "= 0")
-	// first, get some edge cases out of the way when a = 0
-	if a == 0 {
-		return linearPositiveIntegerSolutions(b, c)
-	}
-
-	// at this point, we have a real quadratic equation
-	discriminant := b * b - 4 * a * c
-	// imaginary solutions don't work here
-	if discriminant < 0 {
-		return NoSolutionValue, NoSolutionValue
-	}
-
-	// the discriminant is nonnegative.  However, if its root is not an integer, we have no solutions
-	discriminantRoot := int(math.Sqrt(float64(discriminant)))
-	if discriminantRoot * discriminantRoot != discriminant {
-		return NoSolutionValue, NoSolutionValue
-	}
-
-	couldBe := [2]int{((-1 * b) + discriminantRoot) / (2 * a), ((-1 * b) - discriminantRoot) / (2 * a)}
-	
-	// if the possible solutions don't actually work, set them to NoSolutionValue
-	if a * couldBe[0] * couldBe[0] + b * couldBe[0] + c != 0 {
-		couldBe[0] = NoSolutionValue
-	}
-
-	if a * couldBe[1] * couldBe[1] + b * couldBe[1] + c != 0 {
-		couldBe[1] = NoSolutionValue
-	}
-
-	// if the solutions are valid and the same, set the second one to NoSolutionValue and return them
-	if couldBe[0] >= 0 && couldBe[1] == couldBe[0] {
-		couldBe[1] = NoSolutionValue
-		return couldBe[0], couldBe[1]
-	}
-
-	// if we have two solutions, and they are in descending order, then reorder them
-	if couldBe[0] >= 0 && couldBe[1] >= 0 && couldBe[0] > couldBe[1] {
-		couldBe[0], couldBe[1] = couldBe[1], couldBe[0]
-	}
-	
-	// now just return what we have
-	return couldBe[0], couldBe[1]
-}
-
-// a particle is at the origin at time t when its x, y and z positions are all zero
-func whenAtOrigin(pcl Particle) (int, int) {
-	// the position along an axis is a*t*(t-1) + v*t + p = a*t^2 + (v - a) * t + p
-	xRoot1, xRoot2 := quadraticPositiveIntegerSolutions(pcl.a.x, pcl.v.x - pcl.a.x, pcl.p.x)
-	fmt.Println("Solutions are", xRoot1, xRoot2)
-	yRoot1, yRoot2 := quadraticPositiveIntegerSolutions(pcl.a.y, pcl.v.y - pcl.a.y, pcl.p.y)
-	fmt.Println("Solutions are", yRoot1, yRoot2)
-	zRoot1, zRoot2 := quadraticPositiveIntegerSolutions(pcl.a.z, pcl.v.z - pcl.a.z, pcl.p.z)
-	fmt.Println("Solutions are", zRoot1, zRoot2)
-
-	if xRoot1 == AlwaysZero && yRoot1 == AlwaysZero && zRoot1 == AlwaysZero {
-		return 0, 1
-	}
-
-	answer1, answer2 := NoSolutionValue, NoSolutionValue
-
-	if xRoot1 > 0 && (xRoot1 == yRoot1 || xRoot1 == yRoot2) && (xRoot1 == zRoot1 || xRoot1 == zRoot2) {
-		answer1 = xRoot1
-	}
-	if xRoot2 > 0 && (xRoot2 == yRoot1 || xRoot2 == yRoot2) && (xRoot2 == zRoot1 || xRoot2 == zRoot2) {
-		answer2 = xRoot2
-	}
-	return answer1, answer2
-}
-
-func whenCollide(p1, p2 Particle) (int, int) {
-	difference := Particle{
+func netParticle(p1, p2 Particle) Particle {
+	return Particle{
 		p: Coordinate{x: p1.p.x - p2.p.x, y: p1.p.y - p2.p.y, z: p1.p.z - p2.p.z},
 		v: Coordinate{x: p1.v.x - p2.v.x, y: p1.v.y - p2.v.y, z: p1.v.z - p2.v.z},
 		a: Coordinate{x: p1.a.x - p2.a.x, y: p1.a.y - p2.a.y, z: p1.a.z - p2.a.z},
-	}
-	return whenAtOrigin(difference)
+		zappedRound: NotYet,
+		}
 }
 
-func firstCollide(p1, p2 Particle) int {
-	answer1, answer2 := whenCollide(p1, p2)
-	if answer1 == NoSolutionValue && answer2 == NoSolutionValue {
-		return NoSolutionValue
-	} else if answer2 == NoSolutionValue {
-		return answer1
-	} else if answer1 == NoSolutionValue {
-		return answer2
-	} else if answer1 < answer2 {
-		// return the least of the two answers
-		return answer1
-	} else {
-		return answer2
+func positionFormula(pt Particle, dimension string) (int, int, int) {
+	// the position of the particle is given by the function
+	// (a)(t)(t-1) + (v)(t) + p or
+	// (a)(t)(t) + (v - a)(t) + p
+	switch dimension {
+	case "x": {return pt.a.x, pt.v.x - pt.a.x, pt.p.x}
+	case "y": {return pt.a.y, pt.v.y - pt.a.y, pt.p.y}
+	case "z": {return pt.a.z, pt.v.z - pt.a.z, pt.p.z}
+	// stupid default, but I need to return something
+	default: {return pt.a.z, pt.v.z - pt.a.z, pt.p.z}
 	}
 }
 
 // loop over all of the pairs of particles to determine their earliest collision, if one even exists
 func loadCollisions(particles []Particle) {
-	for i, p := range particles {
+	for i, p1 := range particles {
 		for j, p2 := range particles[(i+1):] {
-			fmt.Println("Comparing particles", p, "and", p2)
-			collisionRound := firstCollide(p, p2)
-			fmt.Println("  collisionRound is", collisionRound)
-			if collisionRound != NoSolutionValue {
-				collisions[collisionRound] = append(collisions[collisionRound], Collision{i, j})
-				if collisionRound > latestCollisionRound {
+			//fmt.Println("Comparing particles", p1, "and", p2)
+			// d = distance, a function of time
+			d := netParticle(p1, p2)
+			collisionSolution := quadratic.CombineSolutions(
+				quadratic.QuadraticPositiveIntegerSolutions(positionFormula(d, "x")),
+				quadratic.QuadraticPositiveIntegerSolutions(positionFormula(d, "y")),
+				quadratic.QuadraticPositiveIntegerSolutions(positionFormula(d, "z")),
+			)
+			if collisionSolution.Never() {continue}
+			// this special case should never happen, because the input data has no particles at the same
+			// starting point
+			if collisionSolution.Always() {
+				fmt.Println("Round ZERO collision for", p1, p2)
+				collisions[0] = append(collisions[0], Collision{p1num: i, p2num: j})
+				if latestCollisionRound == NotYet {
+					latestCollisionRound = 0
+				}
+			}
+			// sometimes, so we have a single-valued solution
+			if collisionSolution.Sometimes() {
+				collisionRound := collisionSolution.Values()[0]
+				fmt.Println("Round", collisionRound, "collision for", p1, p2)
+				collisions[collisionRound] = append(collisions[collisionRound], Collision{p1num: i, p2num: j})
+				if latestCollisionRound < collisionRound {
 					latestCollisionRound = collisionRound
 				}
 			}
@@ -234,6 +153,7 @@ func parseParticle(s string) Particle {
 		case 2: pt.a = parseCoordinates(strippedCoordinates)
 		}	
 	}
+	pt.zappedRound = NotYet
 	return pt
 }
 
