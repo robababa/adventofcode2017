05196ef5a7cf322bf4fea1284c59dac4ad010b3b
Rob Johnson <robj@hightouchinc.com>
2017-12-22 22:16:40 -0600

    Filled out linear solution tests

--- day20/quadratic/quadratic_test.go
+++ day20/quadratic/quadratic_test.go
@@ -2,28 +2,34 @@ package quadratic
 
 import "fmt"
 
+func linearHelper(b, c int) {
+	sol := linearPositiveIntegerSolution(b, c)
+	fmt.Println(b, ",", c, "always(), never(), values() are", sol.always(), sol.never(), sol.values())
+}
+
 func ExampleLinearPositiveIntegerSolution() {
 	// 2x - 6 = 0 : x = 3 is a solution
-	s1 := linearPositiveIntegerSolution(2, -6)
-	fmt.Println("(2, -6) always(), never(), values() are", s1.always(), s1.never(), s1.values())
+	linearHelper(2, -6)
 
 	// 2x - 5 = 0 : x = 2.5 is not an integer, so there are no solutions
-	s2 := linearPositiveIntegerSolution(2, -5)
-	fmt.Println("(2, -5) always(), never(), values() are", s2.always(), s2.never(), s2.values())
+	linearHelper(2, -5)
 
 	// 2x + 6 = 0 : x = -3 is not positive, so there are no solutions
-	s3 := linearPositiveIntegerSolution(2, 6)
-	fmt.Println("(2,  6) always(), never(), values() are", s3.always(), s3.never(), s3.values())
+	linearHelper(2, 6)
 
 	// 2x = 0 : x = 0 is a solution
-	s4 := linearPositiveIntegerSolution(2, 0)
-	fmt.Println("(2,  0) always(), never(), values() are", s4.always(), s4.never(), s4.values())
+	linearHelper(2, 0)
+
+	// 0x = 0 : always is the solution
+	linearHelper(0, 0)
 
 	//Output:
-	//(2, -6) always(), never(), values() are false false [3]
-	//(2, -5) always(), never(), values() are false true []
-	//(2,  6) always(), never(), values() are false true []
-	//(2,  0) always(), never(), values() are false false [0]
+	//2 , -6 always(), never(), values() are false false [3]
+	//2 , -5 always(), never(), values() are false true []
+	//2 , 6 always(), never(), values() are false true []
+	//2 , 0 always(), never(), values() are false false [0]
+	//0 , 0 always(), never(), values() are true false []
+
 }
 
 //func ExampleQuadraticPositiveIntegerSolutions() {
