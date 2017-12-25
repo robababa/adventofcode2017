1a1580a5c70ade8d3cdc823d8221e011e994e29b
Rob Johnson <robj@hightouchinc.com>
2017-12-24 23:06:04 -0600

    Solved day 24 part 2

--- day24/day24.go
+++ day24/day24.go
@@ -26,6 +26,8 @@ type bridgeAndStrength struct {
 
 var strongestBridge = bridgeAndStrength{}
 
+var longestBridges []bridgeAndStrength
+
 func main() {
 	stockpile := parseInput(readInput())
 	//fmt.Println("beam stockpile is:")
@@ -34,11 +36,26 @@ func main() {
 	// recursion easier
 	startingBeam := beam{port1: 0, port2: 0}
 	startingBridge := []beam{startingBeam}
+	//
+	// Part 1
 	buildStrongest(stockpile, startingBridge)
-	// 1287 is too low
 	fmt.Println("Part 1: strongest bridge is", strongestBridge)
 	fmt.Println("Part 1: length of strongest bridge is", len(strongestBridge.bridge) - 1)
 	fmt.Println("Part 1: strength of strongest bridge is", strongestBridge.strength)
+	//
+	// Part 2
+	longestBridges = append(longestBridges, bridgeAndStrength{bridge: startingBridge, strength: 0})
+	buildLongest(stockpile, startingBridge)
+	//fmt.Println("Part 2: longest bridges are", longestBridges)
+	fmt.Println("Part 2: max strength of a longest bridge is", findMaxStrengthOfLongest())
+}
+
+func findMaxStrengthOfLongest() int {
+	answer := 0
+	for _, b := range longestBridges {
+		if b.strength > answer {answer = b.strength}
+	}
+	return answer
 }
 
 // if nextBeam fits the bridge, return it (in the correct direction) and true
@@ -75,6 +92,34 @@ func buildStrongest(stockpile []beamStock, bridge []beam) {
 	if bridgeStrength := strength(bridge); bridgeStrength > strongestBridge.strength {
 		strongestBridge = bridgeAndStrength{bridge: append(bridge), strength: bridgeStrength}
 		//fmt.Println("Strongest bridge now is:", strongestBridge)
+	}
+}
+
+func buildLongest(stockpile []beamStock, bridge []beam) {
+	//fmt.Println("starting with bridge", bridge)
+	for i, b := range stockpile {
+		// if this next beam hasn't been used already
+		if b.available {
+			// and it fits
+			if nextBeam, fits := matchingBeam(bridge, b.piece); fits {
+				// extend the bridge with this beam, mark it unavailable in the stockpile, and recursively
+				// call this function again
+				var updatedStockpile []beamStock
+				updatedStockpile = append(updatedStockpile, stockpile...)
+				updatedStockpile[i].available = false
+				//fmt.Println("comparing stockpile and updatedStockpile", stockpile[i].available, updatedStockpile[i].available)
+				//fmt.Println("New bridge is", append(bridge, nextBeam))
+				buildLongest(updatedStockpile, append(bridge, nextBeam))
+			}
+		}
+	}
+	if len(bridge)  > len(longestBridges[0].bridge) {
+		// Replace the longestBridges array entirely with this bridge
+		longestBridges = append([]bridgeAndStrength{}, bridgeAndStrength{bridge: bridge, strength: strength(bridge)})
+		////fmt.Println("longestBridges now is:", longestBridges)
+	} else if len(bridge) == len(longestBridges[0].bridge) {
+		// append this bridge to the array
+		longestBridges = append(longestBridges, bridgeAndStrength{bridge: bridge, strength: strength(bridge)})
 	}
 }
 
